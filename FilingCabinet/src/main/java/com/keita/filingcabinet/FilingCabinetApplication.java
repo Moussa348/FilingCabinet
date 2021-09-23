34ce3d23367c573e3348d5f3e780f03fed09be7d
Moussa <Moussa.Keita2411@gmail.com>
2021-09-23 01:38:49 -0400

    firstCommit

---
+++ FilingCabinet/src/main/java/com/keita/filingcabinet/FilingCabinetApplication.java
@@ -0,0 +1,13 @@
+package com.keita.filingcabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilingCabinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilingCabinetApplication.class, args);
+    }
+
+}
---
+++ FilingCabinet/src/test/java/com/keita/filingcabinet/FilingCabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keita.filingcabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilingCabinetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
