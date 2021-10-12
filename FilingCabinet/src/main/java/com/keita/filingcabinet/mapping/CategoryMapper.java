191e9a8613a7e78b9dd3d9c6cb4ee60b65e77ecd
Moussa <Moussa.Keita2411@gmail.com>
2021-10-12 16:43:06 -0400

    adding CategoryMapper

--- FilingCabinet/src/main/java/com/keita/filingcabinet/mapping/CategoryMapper.java
+++ FilingCabinet/src/main/java/com/keita/filingcabinet/mapping/CategoryMapper.java
@@ -1,4 +1,26 @@
 package com.keita.filingcabinet.mapping;
 
+import com.keita.filingcabinet.model.dto.CategoryDetailSuperUserView;
+import com.keita.filingcabinet.model.dto.CategoryDetailUserView;
+import com.keita.filingcabinet.model.entity.Category;
+
 public abstract class CategoryMapper {
+
+    public static CategoryDetailUserView toCategoryDetailUserView(Category category){
+        return CategoryDetailUserView.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+    }
+
+    public static CategoryDetailSuperUserView toCategoryDetailSuperUserView(Category category){
+        return CategoryDetailSuperUserView.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .creationDate(category.getCreationDate())
+                .createdBy(category.getCreatedBy())
+                .deactivationDate(category.getDeactivationDate())
+                .deactivatedBy(category.getDeactivatedBy())
+                .build();
+    }
 }
--- FilingCabinet/src/test/java/com/keita/filingcabinet/mapping/CategoryMapperTest.java
+++ FilingCabinet/src/test/java/com/keita/filingcabinet/mapping/CategoryMapperTest.java
@@ -1,4 +1,42 @@
 package com.keita.filingcabinet.mapping;
 
+import com.keita.filingcabinet.mockData.CategoryMockData;
+import com.keita.filingcabinet.model.dto.CategoryDetailSuperUserView;
+import com.keita.filingcabinet.model.dto.CategoryDetailUserView;
+import com.keita.filingcabinet.model.entity.Category;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CategoryMapperTest {
+
+    @Test
+    void toCategoryDetailUserView() {
+        //ARRANGE
+        Category category = CategoryMockData.getListCategoryForRepoTest().get(0);
+
+        //ACT
+        CategoryDetailUserView categoryDetailUserView = CategoryMapper.toCategoryDetailUserView(category);
+
+        //ASSERT
+        assertEquals(category.getId(), categoryDetailUserView.getId());
+        assertEquals(category.getName(), categoryDetailUserView.getName());
+    }
+
+    @Test
+    void toCategoryDetailSuperUserView() {
+        //ARRANGE
+        Category category = CategoryMockData.getListCategoryForRepoTest().get(0);
+
+        //ACT
+        CategoryDetailSuperUserView categoryDetailSuperUserView = CategoryMapper.toCategoryDetailSuperUserView(category);
+
+        //ASSERT
+        assertEquals(category.getId(), categoryDetailSuperUserView.getId());
+        assertEquals(category.getName(), categoryDetailSuperUserView.getName());
+        assertEquals(category.getCreationDate(), categoryDetailSuperUserView.getCreationDate());
+        assertEquals(category.getCreatedBy(), categoryDetailSuperUserView.getCreatedBy());
+        assertEquals(category.getDeactivationDate(), categoryDetailSuperUserView.getDeactivationDate());
+        assertEquals(category.getDeactivatedBy(), categoryDetailSuperUserView.getDeactivatedBy());
+    }
 }
